dbc8663384c01008a737179ece0fc79f390e7c33
Denis Alekseev <alekseevdy@gmail.com>
2011-06-07 19:59:18 +0000

    fb

--- app/controllers/fbstore_controller.rb
+++ app/controllers/fbstore_controller.rb
@@ -5,10 +5,10 @@ class FbstoreController < Spree::BaseController
   layout 'fbstore'
   def catalogue
     p params
-    #oauth = Koala::Facebook::OAuth.new(126581324089558, "d9426496ce5869ff9151d946cd20731b", "http://unfollowers.ru/fbstore/api/")
-    #oauth_access_token = oauth.get_app_access_token
-    #graph = Koala::Facebook::GraphAPI.new(oauth_access_token)
-    graph = Koala::Facebook::GraphAPI.new("126581324089558|Knd9KmEXyz1UozMytst2B_c_Hk4")
+    oauth = Koala::Facebook::OAuth.new(126581324089558, "d9426496ce5869ff9151d946cd20731b", "http://unfollowers.ru/fbstore/api/")
+    oauth_access_token = oauth.get_app_access_token
+    graph = Koala::Facebook::GraphAPI.new(oauth_access_token)
+    #graph = Koala::Facebook::GraphAPI.new("126581324089558|Knd9KmEXyz1UozMytst2B_c_Hk4")
     profile = graph.get_object("me")
     p profile
     @products = Product.all
