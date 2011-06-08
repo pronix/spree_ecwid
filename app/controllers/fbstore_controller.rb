156c8002a5142721dc18a04ff4c403c404b1cbf0
Denis Alekseev <alekseevdy@gmail.com>
2011-06-08 16:58:24 +0400

    fb

--- app/controllers/fbstore_controller.rb
+++ app/controllers/fbstore_controller.rb
@@ -50,7 +50,7 @@ class FbstoreController < Spree::BaseController
     p params[:order_info]
     order = Order.find_by_id(params[:order_info])
     p order
-    render :text => '{"content":[{"title":"[Test Mode] Unicorn","description":"[Test Mode] Own your own mythical beast!","price":' + order.total.to_fb + ',"image_url":"http:\/\/www.facebook.com\/images\/gifts\/21.png","product_url":"http:\/\/www.facebook.com\/images\/gifts\/21.png"}],"method":"payments_get_items"}'
+    render :text => '{"content":[{"title":"[Test Mode] Unicorn","description":"[Test Mode] Own your own mythical beast!","price":' + order.total.to_fb.to_s + ',"image_url":"http:\/\/www.facebook.com\/images\/gifts\/21.png","product_url":"http:\/\/www.facebook.com\/images\/gifts\/21.png"}],"method":"payments_get_items"}'
   end
 
   private
