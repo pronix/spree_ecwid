2d6ee958cdf1209f2588f4b846cc3e02e55f2c0c
Denis Alekseev <alekseevdy@gmail.com>
2011-06-08 20:34:48 +0000

    fb

--- app/controllers/fbstore_controller.rb
+++ app/controllers/fbstore_controller.rb
@@ -50,7 +50,7 @@ class FbstoreController < Spree::BaseController
     req = ""
     if params[:method] == "payments_get_items"
       order = Order.find_by_id(params[:order_info])
-      req = '{"content":[{"title":"[Test Mode] Unicorn","description":"[Test Mode] Own your own mythical beast!","price":' + order.total.to_fb.to_i.to_s + ',"image_url":"http:\/\/www.facebook.com\/images\/gifts\/21.png","product_url":"http:\/\/www.facebook.com\/images\/gifts\/21.png","order_id":"' + order.id + '"}],"method":"payments_get_items"}'
+      req = '{"content":[{"title":"[Test Mode] Unicorn","description":"[Test Mode] Own your own mythical beast!","price":' + order.total.to_fb.to_i.to_s + ',"image_url":"http:\/\/www.facebook.com\/images\/gifts\/21.png","product_url":"http:\/\/www.facebook.com\/images\/gifts\/21.png","order_id":"' + order.id.to_s + '"}],"method":"payments_get_items"}'
     elsif params[:method] == "payments_status_update"
       if params[:status] == 'placed'
         req = {:content=>{:status=>"settled", :order_id=>params[:order_id]}, :method=>"payments_status_update"}.to_json
