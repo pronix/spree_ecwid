ebc9d0a3a2d5a50909b65007c4aedb52bccdce7a
Denis Alekseev <alekseevdy@gmail.com>
2011-06-09 10:59:00 +0400

    fix

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -3,28 +3,25 @@ class CheckoutController < Spree::BaseController
   	p session
   	@methods = []
   	if session[:from_social] == 'vk'
-  	  render :layout => 'vkstore'
 	  @order.rate_hash.each do |shipping_method|
 	    shipping_method[:cost] = shipping_method[:cost].to_vk
 	  end
 	  @item_total = @order.item_total.to_vk
   	  @order_total = @order.total.to_vk
   	  @methods << PaymentMethod.find_by_type("Gateway::Vkontakte")
+  	  render :layout => 'vkstore'
 	elsif session[:from_social] == 'fb'
-	  render :layout => 'fbstore'
 	  @order.rate_hash.each do |shipping_method|
 	    shipping_method[:cost] = shipping_method[:cost].to_fb
 	  end
 	  @item_total = @order.item_total.to_fb
   	  @order_total = @order.total.to_fb
   	  @methods << PaymentMethod.find_by_type("Gateway::Facebook")
+  	  render :layout => 'fbstore'
 	else
 	  @item_total = @order.item_total
   	  @order_total = @order.total
   	  @methods = @order.available_payment_methods
 	end
-	p @item_total
-	p @order_total
-	p "!!!!!!!!!!!!!!!"
   end
 end
